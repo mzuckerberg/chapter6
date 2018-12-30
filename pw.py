fcda586a276802b016b756a3716457523b1d9a52
Mark Gainan <Mooks@Marks-MacBook-Pro.local>
2018-12-30 16:06:55 -0500

    created initial password object

---
+++ pw.py
@@ -0,0 +1,8 @@
+#! python3
+# this is a basic password manager program. warning: not a secure solution student use only
+
+PASSWORDS = {'email': 'f69420ABC123abcXyZ',
+             'blog': '5ixtYn9n3!',
+             'luggage': '123456789'}
+
+
